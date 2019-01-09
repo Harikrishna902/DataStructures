220ebf0282bc33525579a4ee1b92583f4cba609b
Harikrishna902 <46101642+Harikrishna902@users.noreply.github.com>
2019-01-09 12:00:00 +0530

    new

---
+++ NodeUtil.java
@@ -0,0 +1,32 @@
+package bridgelabz.utility;
+
+import datastructures.Node;
+
+public class NodeUtil 
+{
+public String data1;
+public int data;
+public Node nextNode;
+
+public void Node()
+{
+	this.nextNode=null;
+	
+}
+public void setData(String data)
+{
+	this.data1=data;
+	nextNode=null;
+	
+}
+public void setData(int data)
+{
+	this.data=data;
+	nextNode=null;
+	
+}
+public int[] integerFileReader() {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
